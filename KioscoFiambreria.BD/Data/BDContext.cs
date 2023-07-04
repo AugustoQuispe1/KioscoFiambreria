2d735a9714c764c045219d2b2ab00ffe3773c8c9
AugustoQuispe1 <augustoq93@gmail.com>
2023-07-03 22:12:04 -0300

    Add project files.

---
+++ KioscoFiambreria.BD/Data/BDContext.cs
@@ -0,0 +1,20 @@
+﻿using KioscoFiambreria.BD.Data.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KioscoFiambreria.BD.Data
+    //Representacion de la base de datos
+    {
+    public class BDContext :DbContext
+        {
+        public DbSet<Stock> Productos { get; set; }
+
+		public BDContext(DbContextOptions options) : base(options)
+            {
+            }
+        }
+    }
---
+++ KioscoFiambreria.BD/Data/Entity/Stock.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KioscoFiambreria.BD.Data.Entity
+    {
+    public class Stock
+        {
+
+        [Required]
+        public int Id { get; set; }
+
+        public string Producto { get; set; }
+
+        public int Unidad { get; set; }
+
+        }
+    }
